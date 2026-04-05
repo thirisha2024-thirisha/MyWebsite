af8dd40a41b40745ad2407a0af023fdef61af0e1
thirisha2024-thirisha <thirisha2024-thirisha@users.noreply.github.com>
2026-04-05 15:35:54 +0000

    deploy: e920fe8cdc3627eebd1fc8cd4f4f89ec1f786063

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("Hello! Your website is working.");
-alert("Welcome to my SCM project!");
+// Function to show welcome message
+function showMessage() {
+    alert("Welcome to My DevOps Website 🚀");
+}
+
+// Change background color
+function changeColor() {
+    document.body.style.backgroundColor = "#f0f8ff";
+}
+
+// Display current date and time
+function showDateTime() {
+    const now = new Date();
+    document.getElementById("time").innerHTML =
+        "Current Time: " + now.toLocaleString();
+}
