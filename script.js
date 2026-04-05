78119c69f91a8455d76e0c7cd7846ae6acb69daf
thirisha <thirisha.2024@vitstudent.ac.in>
2026-04-05 22:12:57 +0530

    Final CI/CD features added

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
-// Popup automatically when page loads
-window.onload = function () {
-    alert("Welcome to Multi Environment Static Website 🚀");
-};
+// Popup when URL opens
+window.addEventListener("load", function () {
+    alert("Welcome! CI/CD Website Successfully Deployed 🚀");
+});
 
-// Change background color
+// Show Current Time
+function showTime() {
+    const now = new Date();
+    document.getElementById("time").innerHTML =
+        "Current Time: " + now.toLocaleString();
+}
+
+// Change Background Color
 function changeColor() {
     const colors = ["lightblue", "lightgreen", "lightyellow", "lavender"];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 }
-// Show current time
-function showTime() {
-    const now = new Date();
-    document.getElementById("time").innerHTML =
-        "Current Time: " + now.toLocaleString();
+
+// Dark Mode Toggle
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+// Automatic Dark Mode (after 7 PM)
+const hour = new Date().getHours();
+if (hour >= 19) {
+    document.body.classList.add("dark-mode");
 }
